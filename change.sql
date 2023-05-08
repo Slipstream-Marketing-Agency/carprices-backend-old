eb296684982c7934113601f55db84395fd5c8db4
Abdulla Anas <abdullaanasanu@gmail.com>
2023-05-08 17:11:09 +0400

    New car data updated

--- change.sql
+++ change.sql
@@ -6,5 +6,5 @@ ALTER TABLE public.car_brands ADD description text NULL;
 
 # 06/05/2023
 
-update trims t set "fuelConsumption" = 0 where "fuelConsumption" = ''
+update trims t set "fuelConsumption" = 0 where "fuelConsumption" = '';
 ALTER TABLE public.trims ALTER COLUMN "fuelConsumption" TYPE float4 USING "fuelConsumption"::float4;
