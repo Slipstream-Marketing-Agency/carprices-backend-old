8d0040b78fe0c847da2f3bd869321b5ccf339cb9
Abdulla Anas <abdullaanasanu@gmail.com>
2023-05-24 17:56:52 +0400

    Blog APIs added

--- controllers/blog.js
+++ controllers/blog.js
@@ -346,6 +346,53 @@ module.exports.getBlogs = asyncHandler(async (req, res, next) => {
         .json({ blogs: blogs.rows, blogsCount: blogs.count, totalPage: Math.ceil(blogs.count / pageSize) });
 });
 
+module.exports.getBlogsMin = asyncHandler(async (req, res, next) => {
+
+    const { query } = req;
+
+    let isAll = query.isAll ?? false;
+
+    let pageSize = query.pageSize ?? 10;
+    let currentPage = query.currentPage ?? 1;
+    let type = query.type ?? "news";
+    let orderBy = query.orderBy ? [
+        [query.orderBy, "ASC"]
+    ] : [
+        ["publishedAt", "DESC"]
+    ];
+    let where = {
+        published: true,
+        type
+    };
+    if (query.search) {
+        where.title = { [Op.iLike]: `%${query.search}%` }
+    }
+
+    let conditions = {
+        raw: true,
+        attributes: ['id','title', 'slug'],
+        where
+    };
+    if (!isAll) {
+        conditions = {
+            attributes: ['id','title', 'slug'],
+            where,
+            limit: pageSize,
+            offset: (currentPage - 1) * pageSize,
+            order: orderBy,
+            raw: true
+        }
+    }
+
+    let blogs = { rows: [], count: 0 };
+
+    blogs = await Blog.findAndCountAll(conditions);
+
+    res
+        .status(200)
+        .json({ blogs: blogs.rows, blogsCount: blogs.count, totalPage: Math.ceil(blogs.count / pageSize) });
+});
+
 module.exports.getBlogsByModel = asyncHandler(async (req, res, next) => {
 
     const { model } = req.params;
@@ -566,6 +613,124 @@ module.exports.getBlogsByBrand = asyncHandler(async (req, res, next) => {
     if (!isAll) {
         conditions = {
             where,
+            limit: pageSize,
+            offset: (currentPage - 1) * pageSize,
+            order: orderBy,
+            raw: true
+        }
+    }
+
+    let blogs = { rows: [], count: 0 };
+
+    blogs = await Blog.findAndCountAll(conditions);
+
+    blogs.rows = await Promise.all(
+        blogs.rows.map(async blog => {
+            blog.brands = await BlogBrand.findAll({
+                where: {
+                    blogId: blog.id
+                },
+                raw: true
+            });
+            blog.brands = await Promise.all(
+                blog.brands.map(async brand => {
+                    brand = await CarBrand.findByPk(brand.brandId);
+                    return brand;
+                })
+            )
+            blog.categories = await BlogCategory.findAll({
+                where: {
+                    blogId: blog.id
+                },
+                raw: true
+            });
+            blog.categories = await Promise.all(
+                blog.categories.map(async category => {
+                    category = await Category.findByPk(category.categoryId);
+                    return category;
+                })
+            )
+            blog.models = await BlogModel.findAll({
+                where: {
+                    blogId: blog.id
+                },
+                raw: true
+            });
+            blog.models = await Promise.all(
+                blog.models.map(async model => {
+                    model = await Model.findByPk(model.modelId);
+                    return model;
+                })
+            )
+            blog.tags = await BlogTag.findAll({
+                where: {
+                    blogId: blog.id
+                },
+                raw: true
+            });
+            blog.tags = await Promise.all(
+                blog.tags.map(async tag => {
+                    tag = await Tag.findByPk(tag.tagId);
+                    return tag;
+                })
+            )
+            blog.author = await Admin.findByPk(blog.author, {attributes: ["id", "username", "firstName", "lastName", "image"]})
+            return blog;
+        })
+    )
+
+    res
+        .status(200)
+        .json({ blogs: blogs.rows, blogsCount: blogs.count, totalPage: Math.ceil(blogs.count / pageSize) });
+});
+
+module.exports.getBlogsByTag = asyncHandler(async (req, res, next) => {
+
+    const { tag } = req.params;
+    const { query } = req;
+
+    let blogsByTag = await BlogTag.findAll(
+        {
+            where: {
+                tagId: tag
+            },
+            raw: true
+        }
+    )
+
+    let isAll = query.isAll ?? false;
+
+    let pageSize = query.pageSize ?? 10;
+    let currentPage = query.currentPage ?? 1;
+    let type = query.type ?? "news";
+    let orderBy = query.orderBy ? [
+        [query.orderBy, "ASC"]
+    ] : [
+        ["publishedAt", "DESC"]
+    ];
+
+    let blogIds = blogsByTag.map(blog => {
+        return blog.blogId
+    })
+
+    let where = {
+        id: blogIds,
+        type,
+        published: true
+    };
+    if (query.search) {
+        where.title = { [Op.iLike]: `%${query.search}%` }
+    }
+
+    let conditions = {
+        raw: true,
+        attributes: { exclude: ['content'] },
+        where
+    };
+    if (!isAll) {
+        conditions = {
+            where,
+            attributes: { exclude: ['content'] },
             limit: pageSize,
             offset: (currentPage - 1) * pageSize,
             order: orderBy,
--- routes/blog.js
+++ routes/blog.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const { protect } = require("../middlewares/auth");
-const { getAdminBlogs, getBlogs, getBlogBySlug, getBlogsByModel, getBlogsByBrand } = require("../controllers/blog");
+const { getAdminBlogs, getBlogs, getBlogBySlug, getBlogsByModel, getBlogsByBrand, getBlogsMin, getBlogsByTag } = require("../controllers/blog");
 
 router.route("/").get(getBlogs)
+router.route("/min").get(getBlogsMin)
 router.route("/:slug").get(getBlogBySlug)
 router.route("/by-model/:model").get(getBlogsByModel)
-router.route("/by-brand/:brand").get(getBlogsByBrand);
+router.route("/by-brand/:brand").get(getBlogsByBrand)
+router.route("/by-tag/:tag").get(getBlogsByTag)
+
+
+
 
 module.exports = router;
